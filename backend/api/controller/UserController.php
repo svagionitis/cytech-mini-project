57cea20f048e7cb1d0ca17e09285a27aa7583cbf
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2020-09-21 18:57:28 +0300

    backend: update GET requests and add POST/PUT/DELETE requests

--- backend/api/controller/UserController.php
+++ backend/api/controller/UserController.php
@@ -6,19 +6,23 @@ require 'api/gateway/UserGateway.php';
 use Api\Gateway\UserGateway;
 
 /**
- * 
+ *
  */
 class UserController {
 
     private $db;
     private $requestMethod;
+    private $userId;
+    private $email;
 
     private $userGateway;
 
-    public function __construct($db, $requestMethod)
+    public function __construct($db, $requestMethod, $userId, $email)
     {
         $this->db = $db;
         $this->requestMethod = $requestMethod;
+        $this->userId = $userId;
+        $this->email = $email;
 
         $this->userGateway = new UserGateway($db);
     }
@@ -27,7 +31,24 @@ class UserController {
     {
         switch ($this->requestMethod) {
             case 'GET':
-                $response = $this->getAllUsers();
+                if ($this->userId) {
+                    $response = $this->getUserByUserId($this->userId);
+                }
+                else if ($this->email) {
+                    $response = $this->getUserByEmail($this->email);
+                }
+                else {
+                    $response = $this->getUserAll();
+                }
+                break;
+            case 'POST':
+                $response = $this->addUser();
+                break;
+            case 'PUT':
+                $response = $this->updateUserByUserId($this->userId);
+                break;
+            case 'DELETE':
+                $response = $this->deleteUserByUserId($this->userId);
                 break;
             default:
                 $response = $this->notFoundResponse();
@@ -39,11 +60,105 @@ class UserController {
         }
     }
 
-    private function getAllUsers()
+    private function getUserAll()
     {
-        $result = $this->userGateway->getAll();
+        $result = $this->userGateway->getUserAll();
         $response['status_code_header'] = 'HTTP/1.1 200 OK';
         $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getUserByUserId($userId)
+    {
+        $result = $this->userGateway->getUserByUserId($userId);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function getUserByEmail($email)
+    {
+        $result = $this->userGateway->getUserByEmail($email);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function addUser()
+    {
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (! $this->validateUser($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->userGateway->addUser($input);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function updateUserByUserId($userId)
+    {
+        $result = $this->userGateway->getUserByUserId($userId);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        if (! $this->validateUser($input)) {
+            return $this->unprocessableEntityResponse();
+        }
+        $this->userGateway->updateUserByUserId($userId, $input);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function deleteUserByUserId($userId)
+    {
+        $result = $this->userGateway->getUserByUserId($userId);
+        if (! $result) {
+            return $this->notFoundResponse();
+        }
+        $this->userGateway->deleteUserByUserId($userId);
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = null;
+        return $response;
+    }
+
+    private function validateUser($input)
+    {
+        if (! isset($input['FirstName'])) {
+            return false;
+        }
+        if (! isset($input['LastName'])) {
+            return false;
+        }
+        if (! isset($input['Email'])) {
+            return false;
+        }
+        if (! isset($input['TravelDateStart'])) {
+            return false;
+        }
+        if (! isset($input['TravelDateEnd'])) {
+            return false;
+        }
+        if (! isset($input['TravelReason'])) {
+            return false;
+        }
+        return true;
+    }
+
+    private function unprocessableEntityResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 422 Unprocessable Entity';
+        $response['body'] = json_encode([
+            'error' => 'Invalid input'
+        ]);
         return $response;
     }
 
--- backend/api/gateway/UserGateway.php
+++ backend/api/gateway/UserGateway.php
@@ -8,7 +8,7 @@ namespace Api\Gateway;
  * * get a specific user
  * * add a user
  * * update a user
- * * delete a user 
+ * * delete a user
  */
 class UserGateway {
 
@@ -19,12 +19,15 @@ class UserGateway {
         $this->db = $db;
     }
 
-    public function getAll()
+    /**
+     * Get all users from DB
+     */
+    public function getUserAll()
     {
         $statement = "
-            SELECT 
-                UserID, FirstName, LastName, Email, TravelDateStart, TravelDateEnd, TravelReason 
-            FROM 
+            SELECT
+                UserID, FirstName, LastName, Email, TravelDateStart, TravelDateEnd, TravelReason
+            FROM
                 user;
         ";
 
@@ -32,9 +35,140 @@ class UserGateway {
             $statement = $this->db->query($statement);
             $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
             return $result;
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             exit($e->getMessage());
         }
     }
+
+    /**
+     * Get user by a specific UserID
+     */
+    public function getUserByUserId($userId)
+    {
+        $statement = "
+            SELECT
+                UserID, FirstName, LastName, Email, TravelDateStart, TravelDateEnd, TravelReason
+            FROM
+                user
+            WHERE
+                UserID = ?;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array($userId));
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    /**
+     * Get user by a specific Email
+     */
+    public function getUserByEmail($email)
+    {
+        $statement = "
+            SELECT
+                UserID, FirstName, LastName, Email, TravelDateStart, TravelDateEnd, TravelReason
+            FROM
+                user
+            WHERE
+                Email = ?;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array($email));
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    /**
+     * Add a new user to db (SQL INSERT)
+     */
+    public function addUser(Array $input)
+    {
+        $statement = "
+            INSERT INTO user
+                (FirstName, LastName, Email, TravelDateStart, TravelDateEnd, TravelReason)
+            VALUES
+                (:FirstName, :LastName, :Email, :TravelDateStart, :TravelDateEnd, :TravelReason);
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                'FirstName' => $input['FirstName'],
+                'LastName'  => $input['LastName'],
+                'Email' => $input['Email'],
+                'TravelDateStart' => $input['TravelDateStart'],
+                'TravelDateEnd' => $input['TravelDateEnd'],
+                'TravelReason' => $input['TravelReason'],
+            ));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    /**
+     * Update a user with a specific UserId (SQL UPDATE)
+     */
+    public function updateUserByUserId($userId, Array $input)
+    {
+        $statement = "
+            UPDATE user
+            SET
+                FirstName = :FirstName,
+                LastName  = :LastName,
+                Email = :Email,
+                TravelDateStart = :TravelDateStart,
+                TravelDateEnd = :TravelDateEnd,
+                TravelReason = :TravelReason
+            WHERE
+                UserId = :UserId;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                'UserId' => (int) $userId,
+                'FirstName' => $input['FirstName'],
+                'LastName'  => $input['LastName'],
+                'Email' => $input['Email'],
+                'TravelDateStart' => $input['TravelDateStart'],
+                'TravelDateEnd' => $input['TravelDateEnd'],
+                'TravelReason' => $input['TravelReason'],
+            ));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    /**
+     * Delete a user with a specific UserId (SQL DELETE)
+     */
+    public function deleteUserByUserId($userId)
+    {
+        $statement = "
+            DELETE FROM user
+            WHERE UserId = :UserId;
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array('UserId' => $userId));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
 }
 ?>
